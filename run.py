e4333eeb27b5d33442ac683a8675ab73729e10bf
Narender Singh <narendersingh@duck.com>
2022-06-10 23:37:01 +0000

    added demo

--- run.py
+++ run.py
@@ -8,22 +8,26 @@ import os
 import time
 
 
-def main(stdscr):
+def main():
 
-    print("Executing main...")
-    time.sleep(10)
+    stdscr = curses.initscr()
+    curses.start_color()
 
     # Clear and refresh the screen for a blank canvas
     stdscr.clear()
     stdscr.refresh()
 
-    # add text top screen
-    stdscr.addstr(0, 0, "Sree is here!", curses.color_pair(1))
-
+    # # add text top screen
+    curses.noecho()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    stdscr.addstr(10, 10, "Sree is here!", curses.color_pair(1))
+    stdscr.refresh()
+    curses.echo()
+    time.sleep(1)
     k = stdscr.getch()
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("TERM", "linux")
+    os.environ.setdefault("TERM", "xterm")
     os.environ.setdefault("TERMINFO", "/etc/terminfo")
-    curses.wrapper(main)
+    main()
